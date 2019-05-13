09e900df19e808d8aab2afe008b883b3bb3fef95
Yeshi <tyeshi777@yahoo.com>
2019-05-13 16:39:35 -0700

    durability -= 5, don't forget to save the result

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,15 +2,24 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  let enhancement = item.enhancement;
+
+  enhancement >= 0 && enhancement < 20 ? (enhancement += 1) : enhancement;
+  return { ...item, enhancement: enhancement };
 }
 
 function fail(item) {
-  return { ...item };
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+  enhancement < 15 ? (durability -= 5) : (durability -= 10);
+  enhancement > 16 ? (enhancement -= 1) : enhancement;
+
+  let newItem = item;
+  return { ...newItem, enhancement: enhancement, durability: durability };
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,42 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancer.js", () => {
+  describe("succeed() method", () => {
+    it("adds 1 to the enhancement", () => {
+      const item = { enhancement: 16 };
+      const actual = enhancer.succeed(item);
+      console.log("FUNCTION", actual);
+      expect(actual.enhancement).toBe(17);
+      console.log("ACTUAL", actual.enhancement);
+    });
+    it("enhancement equals 20 doesn't increment", () => {
+      const item = {
+        enhancement: 20
+      };
+      const actual = enhancer.succeed(item);
+
+      expect(actual.enhancement).toBe(20);
+    });
+  });
+
+  describe("fail() method", () => {
+    it("decreases the durability by 10", () => {
+      const item = { enhancement: 18, durability: 80 };
+      const actual = enhancer.fail(item);
+
+      expect(actual.durability).toBe(70);
+    });
+    it("decreases the durability by 5", () => {
+      const item = { enhancement: 10, durability: 60 };
+      const actual = enhancer.fail(item);
+
+      expect(actual.durability).toBe(55);
+    });
+    it("decreases the enhancement by 1", () => {
+      const item = { enhancement: 18 };
+      const actual = enhancer.fail(item);
+
+      expect(actual.enhancement).toBe(17);
+    });
+  });
+});
